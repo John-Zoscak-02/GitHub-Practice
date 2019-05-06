63d86bd88b9ea1b547102dde4460e0f03b2d4c10
John Z <ZoscakJM20@pwcs-edu.org>
2019-05-06 14:01:47 -0400

    My project will find a random number within a given range

--- Git_Hub_Practice/src/GitHubPractice.java
+++ Git_Hub_Practice/src/GitHubPractice.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
-
 public class GitHubPractice {
     public static void main(String args) {
         System.out.println( "What is the first number?" );
         Scanner scan1 = new Scanner( System.in )
         System.out.println( "What is the second number?" );
         Scanner scan2 = new Scanner( System.in );
-        System.out.println( "The average of the two numbers is: " + ((scan1.nextInt() + scan2.nextInt())/2)  )
+        int first = scan1.nextInt();
+        System.out.println( "Your number is: " + ((int)(Math.random()*(first - scan2.nextInt())) + first) )
     }
 }
