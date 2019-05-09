df0121c3ac715076f89a4bff9855bcf4851f0894
Daniel Tolbert <danielchristopher19@gmail.com>
2019-05-09 10:03:29 -0400

    NOW it is fixed. With comments to boot..!

--- Git_Hub_Practice/src/GitHubPractice.java
+++ Git_Hub_Practice/src/GitHubPractice.java
@@ -1,12 +1,18 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class GitHubPractice {
     public static void main(String[] args) {
-        Scanner scan1 = new Scanner( System.in );
+        Random random = new Random(  ); //Ditched the math.random in favor of Random class
+
+        Scanner scan1 = new Scanner( System.in );   // From two scanners to one
         System.out.println( "What is the first number?" );
-        double first = scan1.nextDouble();
+        int first = scan1.nextInt(); //Changed from double to int
         System.out.println( "What is the second number?" );
-        double second = scan1.nextDouble();
-        System.out.println( "Your number is: " + (first + second) / 2);
+        int second = scan1.nextInt();
+        if ( second < first ) { //Throws an error if the high bound ain't high
+            System.err.print( "Error: the high bound must be larger than the low bound" );
+        }
+        System.out.println( "Your number is: " + (random.nextInt( second - first ) + first) ) ;
     }
 }
